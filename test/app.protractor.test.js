47acad4e7dc46ecab48d894c7a7e01b778e0cbcd
Jonathan Conway <jonathan.conway@gmail.com>
2016-03-08 19:29:55 +1100

    implement sort & end-to-end test for it

--- test/app.protractor.test.js
+++ test/app.protractor.test.js
@@ -1,5 +1,6 @@
 describe('AppController', function () {
     var expect = require('chai').expect;
+
     it('should filter people by firstName', function() {    
         browser.get('http://localhost:3000/index.html').then(function () {
             var searchAny = element(by.model('search.firstName'));
@@ -13,6 +14,28 @@ describe('AppController', function () {
                 .each(function(elem, i) {
                     return elem.getText().then(function(text) {
                         expect(text).to.equal(expectedNames[i]);
+                    });
+                });
+        });
+    });
+
+    it('should sort people if sort button selected', function() {    
+        browser.get('http://localhost:3000/index.html').then(function () {
+            var searchBox = element(by.model('search.firstName'));
+            searchBox.clear();
+
+            var sortButton = element(by.css('.sort'));
+            sortButton.click();
+
+            var expectedNames = ['Eric', 'Hayley', 'Lucy'];
+            var i = 0;
+            var value = element
+                .all(by.css('.name'))
+                .each(function(elem, i) {
+                    return elem.getText().then(function(text) {
+                        if (i < expectedNames.length) {
+                            expect(text).to.equal(expectedNames[i]);
+                        }
                     });
                 });
         });
