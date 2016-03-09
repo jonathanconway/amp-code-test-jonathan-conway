f1537c8de2b08c8e790f1021941dc833456af8cf
Jonathan Conway <jonathan.conway@gmail.com>
2016-03-09 11:18:18 +1100

    fix data

--- data.js
+++ data.js
@@ -3,42 +3,42 @@ var data = [
         'id': '1',
         'firstName': 'Sean',
         'lastName': 'Kerr',
-        'picture': 'img/sean.jpg',
+        'picture': 'img/sean.png',
         'Title': 'Senior Developer'
     },
     {
         'id': '2',
         'firstName': 'Yaw',
         'lastName': 'Ly',
-        'picture': 'img/yaw.jpg',
+        'picture': 'img/yaw.png',
         'Title': 'AEM Magician'
     },
     {
         'id': '3',
         'firstName': 'Lucy',
         'lastName': 'Hehir',
-        'picture': 'img/lucy.jpg',
+        'picture': 'img/lucy.png',
         'Title': 'Scrum master'
     },
     {
         'id': '4',
         'firstName': 'Rory',
         'lastName': 'Elrick',
-        'picture': 'img/rory.jpg',
+        'picture': 'img/rory.png',
         'Title': 'AEM Guru'
     },
     {
         'id': '5',
         'firstName': 'Eric',
         'lastName': 'Kwok',
-        'picture': 'img/eric.jpg',
+        'picture': 'img/eric.png',
         'Title': 'Technical Lead'
     },
     {
         'id': '6',
         'firstName': 'Hayley',
         'lastName': 'Crimmins',
-        'picture': 'img/hayley.jpg',
+        'picture': 'img/hayley.png',
         'Title': 'Dev Manager'
     }
 ];
